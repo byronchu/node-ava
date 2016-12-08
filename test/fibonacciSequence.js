7d7b3f81ff2b07c73d174ed3900f27c079715d61
Byron Chu <byronchu@Byrons-MacBook-Pro.local>
2016-12-08 12:46:26 +1100

    add fibbonacciSequence and ava

---
+++ test/fibonacciSequence.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import fibonacciSequence from '../lib/fibonacciSequence';
+
+
+
+test('first 2', (t) => {
+  t.deepEqual(
+    fibonacciSequence(2),
+    [1, 1]
+  );
+});
+
+test('first 3', (t) =>  {
+  t.deepEqual(
+    fibonacciSequence(3),
+    [1, 1, 2]
+  );
+});
+
+test('first 1', t =>  {
+  t.deepEqual(
+    fibonacciSequence(1),
+    [1]
+  );
+});
+
+test('first 0', (t) =>  {
+  t.deepEqual(
+    fibonacciSequence(0),
+    []
+  );
+});
